83c6854ca529166c4091b9eeb3675284b61a5285
Tahlil1 <bsse0803@iit.du.ac.bd>
2019-10-08 10:12:35 +0600

    added basic component for local

--- SourceCode/MobileApp/PiperNet/src/app/app.module.ts
+++ SourceCode/MobileApp/PiperNet/src/app/app.module.ts
@@ -16,7 +16,7 @@ import { FileTypeIconService } from "./services/file-type-icon.service";
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [
     StatusBar,
     SplashScreen,
