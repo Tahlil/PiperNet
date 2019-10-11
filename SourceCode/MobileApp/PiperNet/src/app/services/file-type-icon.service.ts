de9e21c49fb30a51bf64094cf9db179c05d0314b
Tahlil1 <bsse0803@iit.du.ac.bd>
2019-10-11 14:40:38 +0600

    ion slide item works

--- SourceCode/MobileApp/PiperNet/src/app/services/file-type-icon.service.ts
+++ SourceCode/MobileApp/PiperNet/src/app/services/file-type-icon.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient  } from '@angular/common/http';
 
-const fileTypeIconPath = "../assets/img/fileIcons/";
+const fileTypeIconPath = "../../../assets/img/fileIcons/";
 const fileIconListTxtPath = "../assets/allFileTypes.txt"
 
 @Injectable({
--- SourceCode/MobileApp/PiperNet/src/app/tab1/local-world/local-world.component.ts
+++ SourceCode/MobileApp/PiperNet/src/app/tab1/local-world/local-world.component.ts
@@ -1,65 +1,103 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { ModalController } from "@ionic/angular";
+import { IonItemSliding } from '@ionic/angular';
+
 
 import { FileModalComponent } from "./file-modal/file-modal.component";
 import { FileService } from "../../services/file.service";
+import { FileTypeIconService } from "../../services/file-type-icon.service";
+
 
 @Component({
-  selector: 'app-local-world',
-  templateUrl: './local-world.component.html',
-  styleUrls: ['./local-world.component.scss'],
+  selector: "app-local-world",
+  templateUrl: "./local-world.component.html",
+  styleUrls: ["./local-world.component.scss"]
 })
 export class LocalWorldComponent implements OnInit {
+  uploadedFiles: any[];
+  downloadedFiles: any[];
 
-  uploadedFiles:any[];
-  downloadedFiles:any[];
-
-  constructor(private fileService: FileService, private modalCtrl: ModalController) { 
+  constructor(
+    private fileIconService: FileTypeIconService,
+    private fileService: FileService,
+    private modalCtrl: ModalController
+  ) {
     this.downloadedFiles = [];
     this.uploadedFiles = [];
     let uploadedfilesPromise = this.fileService.getUploadedFiles();
     this.getUploadedFiles(uploadedfilesPromise);
     let downloadedfilesPromise = this.fileService.getUploadedFiles();
     this.getUploadedFiles(downloadedfilesPromise);
-    
   }
+
   @Output() changeView = new EventEmitter();
 
   ngOnInit() {}
 
-  goBack(){
+  goBack() {
     this.changeView.emit("local");
   }
 
-  private getUploadedFiles(uploadedfilesPromise){
+  private getUploadedFiles(uploadedfilesPromise) {
     let index = 0;
     uploadedfilesPromise.then(res => {
       for (const file of res) {
         console.log("File promise: " + file);
         file.then(fileInfo => {
           this.uploadedFiles[index] = fileInfo;
-          console.log("File Info: " + fileInfo.name + " " + fileInfo.type + " " + fileInfo.size + " " + fileInfo.path);
-          index++;          
+          console.log(
+            "File Info: " +
+              fileInfo.name +
+              " " +
+              fileInfo.type +
+              " " +
+              fileInfo.size +
+              " " +
+              fileInfo.path
+          );
+          index++;
         });
       }
     });
   }
 
-  private getDownloadedFiles(downloadedfilesPromise){
+  private getDownloadedFiles(downloadedfilesPromise) {
     let index = 0;
     downloadedfilesPromise.then(res => {
       for (const file of res) {
         console.log("File promise: " + file);
         file.then(fileInfo => {
           this.uploadedFiles[index] = fileInfo;
-          console.log("File Info: " + fileInfo.name + " " + fileInfo.type + " " + fileInfo.size + " " + fileInfo.path);
-          index++;          
+          console.log(
+            "File Info: " +
+              fileInfo.name +
+              " " +
+              fileInfo.type +
+              " " +
+              fileInfo.size +
+              " " +
+              fileInfo.path
+          );
+          index++;
         });
       }
-    }); 
+    });
   }
 
-  openModal(action:string){
+  getImagePath(fileType: string): string {
+    return this.fileIconService.getFileImagePath(fileType);
+  }
+
+  onDelete(type:string, fileName: string, slidingItem: IonItemSliding){
+    slidingItem.close();
+    this.deleteFile(type, fileName);
+  }
+
+  private deleteFile(type:string, fileName:string){
+    // this.
+  }
+
+  openModal(action: string) {
     this.modalCtrl
       .create({
         component: FileModalComponent,
@@ -71,10 +109,9 @@ export class LocalWorldComponent implements OnInit {
       })
       .then(resultData => {
         console.log(resultData.data, resultData.role);
-        if (resultData.role === 'confirm') {
+        if (resultData.role === "confirm") {
           console.log(action + "ed File");
         }
       });
   }
-
 }
