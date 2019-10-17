19aebb262997581af5f6edf02020fef1221292b8
Tahlil1 <bsse0803@iit.du.ac.bd>
2019-10-17 21:35:28 +0600

    login and sign up ui added

--- SourceCode/MobileApp/PiperNet/src/app/auth/auth.page.ts
+++ SourceCode/MobileApp/PiperNet/src/app/auth/auth.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -6,10 +7,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.page.scss'],
 })
 export class AuthPage implements OnInit {
+  @Input() isSigningIn:boolean;
+  passwordMatch:boolean; 
+  emailValid:boolean;
 
-  constructor() { }
+  constructor() {  
+    this.isSigningIn = true;
+    this.passwordMatch = true;
+    this.emailValid = true;
+  }
 
   ngOnInit() {
   }
 
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const username = form.value.username;
+    const password = form.value.password;
+    let confirmPass, email;
+    if(this.isSigningIn){
+      email = form.value.email;
+      confirmPass = form.value.confirmPassword;
+      if(password !== confirmPass){
+        this.passwordMatch = false;
+        return;
+      }
+
+      this.passwordMatch = true;
+    }
+    console.log(email, password);
+    if (this.isSigningIn) {
+      // Send a request to login servers
+    } else {
+      // Send a request to signup servers
+    }
+  }
+
+  onSwitchAuthMode(){
+    this.isSigningIn = !this.isSigningIn;    
+  }
+
+  test(emailCtrl): boolean{
+    console.log(emailCtrl);
+    return true;
+  }
 }
