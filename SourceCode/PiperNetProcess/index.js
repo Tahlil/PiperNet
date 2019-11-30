94b4e0272ea478f43aa109d5ba1ea31e3e766bed
Tahlil1 <bsse0803@iit.du.ac.bd>
2019-11-30 12:24:30 +0600

    set up UDP

--- SourceCode/PiperNetProcess/index.js
+++ SourceCode/PiperNetProcess/index.js
@@ -2,5 +2,22 @@
 const fs = require('fs');
 const bencode = require('bencode');
 
+const dgram = require('dgram');
+const Buffer = require('buffer').Buffer;
+const urlParse = require('url').parse;
+
 const torrent = bencode.decode(fs.readFileSync('puppy.torrent'));
-console.log(torrent.announce.toString('utf8'));
+
+const url = urlParse(torrent.announce.toString('utf8'));
+
+const socket = dgram.createSocket('udp4');
+
+const myMsg = Buffer.from('hello?', 'utf8');
+
+socket.send(myMsg, 0, myMsg.length, url.port, url.host, () => {});
+
+socket.on('message', msg => {
+  console.log('message is', msg);
+});
+
+console.log("FIN");
