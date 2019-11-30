1ccc6abe1f285e3ccc1b4b2042f4ced5b658ac44
Tahlil1 <bsse0803@iit.du.ac.bd>
2019-11-30 12:10:58 +0600

    added bencode

--- SourceCode/PiperNetProcess/index.js
+++ SourceCode/PiperNetProcess/index.js
@@ -1,4 +1,6 @@
 'use strict';
 const fs = require('fs');
-const torrent = fs.readFileSync('puppy.torrent');
-console.log(torrent.toString('utf8'));
+const bencode = require('bencode');
+
+const torrent = bencode.decode(fs.readFileSync('puppy.torrent'));
+console.log(torrent.announce.toString('utf8'));
