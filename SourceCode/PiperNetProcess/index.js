bfdeeade7e44100a808c0b5932fd01314da4a175
Tahlil1 <bsse0803@iit.du.ac.bd>
2019-11-30 13:58:39 +0600

    move tracker code to different file

--- SourceCode/PiperNetProcess/index.js
+++ SourceCode/PiperNetProcess/index.js
@@ -1,23 +1,11 @@
 'use strict';
+
 const fs = require('fs');
 const bencode = require('bencode');
-
-const dgram = require('dgram');
-const Buffer = require('buffer').Buffer;
-const urlParse = require('url').parse;
+const tracker = require('./tracker');
 
 const torrent = bencode.decode(fs.readFileSync('puppy.torrent'));
 
-const url = urlParse(torrent.announce.toString('utf8'));
-
-const socket = dgram.createSocket('udp4');
-
-const myMsg = Buffer.from('hello?', 'utf8');
-
-socket.send(myMsg, 0, myMsg.length, url.port, url.host, () => {});
-
-socket.on('message', msg => {
-  console.log('message is', msg);
+tracker.getPeers(torrent, peers => {
+  console.log('list of peers: ', peers);
 });
-
-console.log("FIN");
